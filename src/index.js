c9225bb93f48f835431a602c3c878f151a1a1829
Viktor Sobolevskiy <viktor.sobolevskiy@netent.com>
2017-11-14 02:37:26 +0200

    add randomizer for chart data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as d3 from "d3";
 
+/**
+ * Market changes imitator.
+ */
+class DataProvider {
+
+    constructor() {
+        this.currentValue = 100;
+        this.changingRange = 1.5;
+        this.changingFrequency = 400;
+        this.intervalId = null;
+        this.startProducing();
+    }
+
+    randomBetween(a, b) {
+        return Math.random() * (b - a) + a;
+    }
+
+    produceValue() {
+        const sign = Math.round(Math.random()) ? 1 : -1;
+        const newValue = this.currentValue +
+            this.randomBetween(0, this.changingRange) * sign;
+
+        this.currentValue = +newValue.toFixed(3);
+
+        return this.currentValue;
+    }
+
+    startProducing() {
+
+        if (!this.intervalId) {
+            this.intervalId = setInterval(
+                () => {
+                    console.log(this.produceValue());
+                },
+                this.changingFrequency
+            );
+        }
+    }
+
+    stopProducing() {
+
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+}
+
 class Graph extends React.Component {
 
     constructor(props) {
@@ -25,12 +73,28 @@ class Graph extends React.Component {
 
     componentDidMount() {
         const svg = d3.select("#graph");
+        const width = this.state.width;
+        const height = this.state.height;
 
-        // d3 chart initialization here
+        svg.append("line")
+            .attr("x1", 0)
+            .attr("y1", height >> 1)
+            .attr("x2", width)
+            .attr("y2", height >> 1)
+            .attr("stroke", "#504f50")
+            .attr("stroke-width", 3);
+
+        svg.append("line")
+            .attr("x1", width >> 1)
+            .attr("y1", 0)
+            .attr("x2", width >> 1)
+            .attr("y2", height)
+            .attr("stroke", "#504f50")
+            .attr("stroke-width", 3);
     }
 }
 
 ReactDOM.render(
-    <Graph width={400} height={350} />,
+    <Graph />,
     document.getElementById("root")
 );
